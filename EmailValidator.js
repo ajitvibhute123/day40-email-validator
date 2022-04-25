1f03fc2eb2011fc9fa2f0408fb327c1aec485095
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-04-25 20:51:59 +0530

    Added : Handling optional part of the email

--- EmailValidator.js
+++ EmailValidator.js
@@ -32,3 +32,11 @@ arguments.splice(0,2);
         console.log(element + " :", emailRegex.test(element));
     });
 }
+
+// UC 5
+{
+    let emailRegex = RegExp('^[a-z]+([\.\+\_\-][a-z]+)?@[a-z]+.[a-z]+(\.[a-z]{2})?$');
+    arguments.forEach(element => {
+        console.log(element + " :", emailRegex.test(element));
+    });
+}
